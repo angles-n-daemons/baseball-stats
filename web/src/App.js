0b97c18a5fb7232f57effdbc079bb874059c9451
Brian Dillmann <dillmann.brian@gmail.com>
2023-08-07 15:36:49 -0400

    try a separate csv parser

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,37 @@
+import { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import csvToObjects from './utils/csvToObjects.js';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        async function getData() {
+            const response = await fetch('stats.csv');
+            const result = await response.text();
+            csvToObjects(result);
+        }
+        getData();
+    });
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                   Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                   className="App-link"
+                   href="https://reactjs.org"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >
+                   Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
